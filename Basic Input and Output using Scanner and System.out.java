64a028dfde010c50f49ae033bdec9409b6b7c563
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-07 19:00:41 +0600

    Create Basic Input and Output using Scanner and System.out.java
    User input and output Examples. There have two programs

---
+++ Basic Input and Output using Scanner and System.out.java
@@ -0,0 +1,24 @@
+                                    // User inputs and output Examples                                                                                                       
+
+// Scanner: A class in java.util package used for obtaining input of primitive types.
+
+import java.util.Scanner; //
+
+public class InputExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
+
+// System.out: Used to print output to the console.
+
+public class OutputExample {
+    public static void main(String[] args) {
+        System.out.println("This is an output example.");
+    }
+}
+
