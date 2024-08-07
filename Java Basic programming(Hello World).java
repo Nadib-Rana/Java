d077f982b0929e74a6dfe62704e70aefe0af4983
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-07 18:52:24 +0600

    Update Java Basic programming(Hello World).java
    first java programming. print hello world

--- Java Basic programming(Hello World).java
+++ Java Basic programming(Hello World).java
@@ -1,6 +1,6 @@
 // This is the first Java code. Print |Hello World|
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Hello, World!"); //If you are NetBeans user. You can use shortcut: sout -> tab
+      System.out.println("Hello, World!"); //If you are NetBeans user. You can use shortcut: sout -> tab 
     }
 }
