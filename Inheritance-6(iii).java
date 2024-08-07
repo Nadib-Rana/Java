614c9ad5e1344fc5514f8ef6d105a53f12df7dad
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-07 22:14:29 +0600

    Update Inheritance-6(iii).java
    Inheritance

--- Inheritance-6(iii).java
+++ Inheritance-6(iii).java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.eat(); // Output: This animal eats food.
-        dog.bark(); // Output: The dog barks.
+        dog.bark(); // Output: The dog barks. 
     }
 }
 
