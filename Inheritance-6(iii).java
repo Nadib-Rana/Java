e7688491739cf348bc977de5d6eb7a62605244bd
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-07 22:13:27 +0600

    Create Inheritance-6(iii).java

---
+++ Inheritance-6(iii).java
@@ -0,0 +1,25 @@
+// Definition: The mechanism in Java by which one class is allowed to inherit the features (fields and methods) of another class.
+// Usage: Achieved by using the extends keyword.
+
+// Base class
+public class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+// Derived class
+public class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat(); // Output: This animal eats food.
+        dog.bark(); // Output: The dog barks.
+    }
+}
+
